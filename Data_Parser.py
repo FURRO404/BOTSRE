5e2489ef1f21975359acca98dc163d81104c6f4d
NotSoToothless <67082114+FURRO404@users.noreply.github.com>
2025-02-25 01:59:19 +0000

    Adding some name normalization

--- Data_Parser.py
+++ Data_Parser.py
@@ -1,6 +1,11 @@
+import asyncio
 import csv
 import io
+import re
+import string
+
 import aiofiles
+
 
 async def match_cdk_to_actual_name(vehicle_name):
     async with aiofiles.open('WT-DATA.csv', mode='r', encoding='utf-8') as f:
@@ -11,10 +16,17 @@ async def match_cdk_to_actual_name(vehicle_name):
     reader = csv.DictReader(csv_file)
     for row in reader:
         if row.get('wk_name') == vehicle_name.lower():
-            return row.get('alt_name')
+            alt_name = row.get('alt_name')
+            return await normalize_name(alt_name) if alt_name else vehicle_name
     return vehicle_name
 
-#import asyncio
-#vehicle = "germ_pzkpfw_VI_ausf_h1_tiger"
-#alt_name = asyncio.run(match_cdk_to_actual_name(vehicle))
-#print(alt_name)
+async def normalize_name(name: str) -> str:
+    name = name.replace('_', ' ')
+    allowed_punct = re.escape(string.punctuation)
+    pattern = f'[^A-Za-z0-9{allowed_punct} ]'
+    # Remove characters that don't match the allowed set
+    normalized = re.sub(pattern, '', name)
+    return normalized
+
+#vehicle = "germ_pzkpfw_VI_ausf_h1_tiger_west"
+#print(asyncio.run(match_cdk_to_actual_name(vehicle)))
